c8dd3b24e9aa42882554f88a0d0e8602dcccb3d5
guavacadodev <jakewoodall001@gmail.com>
2024-10-25 14:10:27 -0700

    Initial Commit

---
+++ iBite/iBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBiteApp.swift
+//  iBite
+//
+//  Created by Jake Woodall on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct iBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
